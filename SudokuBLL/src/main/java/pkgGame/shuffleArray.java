5217ad0299292ef3f25be6097ae5487e7445f500
Mohammed Alshuwaykhat <Mohammex@udel.edu>
2018-09-30 17:57:11 -0400

    fillDiagonal and shuffleArray

--- SudokuBLL/src/main/java/pkgGame/shuffleArray.java
+++ SudokuBLL/src/main/java/pkgGame/shuffleArray.java
@@ -1,1 +1,18 @@
+package pkgGame;
+import java.util.List;
 
+class shuffleArray {
+	
+	private List<Integer> shuffleArray(int[] ar) {
+		
+		List<Integer> list = new ArrayList<>;
+		
+		for (i : ar)
+			list.add(i);
+		
+		Collections.shuffle(list);
+		
+		return list;
+		
+	}
+}
