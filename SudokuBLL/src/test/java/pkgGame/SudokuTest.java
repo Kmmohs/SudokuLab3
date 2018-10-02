aa997a3dfbe7379b761bce3562792fbe21b99f17
kenypow <kenypow@udel.edu>
2018-10-01 23:10:55 -0400

    commiting test

--- SudokuBLL/src/test/java/pkgGame/SudokuTest.java
+++ SudokuBLL/src/test/java/pkgGame/SudokuTest.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import pkgHelper.LatinSquare;
+
 public class SudokuTest {
 
 	@Test
@@ -317,6 +319,25 @@ public class SudokuTest {
 		return hasDuplicates;
 	}
 	@Test
-	
-
+	public void shuffleArray_Test() {
+		int[][] MySquare = { { 0, 3, 4, 6, 7, 8, 9, 1, 2 }, { 6, 7, 2, 1, 9, 5, 3, 4, 8 }, { 1, 9, 8, 3, 4, 2, 5, 6, 7 },
+				{ 8, 5, 9, 7, 6, 1, 4, 2, 3 }, { 4, 2, 6, 8, 5, 3, 7, 9, 1 }, { 7, 1, 3, 9, 2, 4, 8, 5, 6 },
+				{ 9, 6, 1, 5, 3, 7, 2, 8, 4 }, { 2, 8, 7, 4, 1, 9, 6, 3, 5 }, { 3, 4, 5, 2, 8, 6, 1, 7, 9 }  };
+		Sudoku s1 = null;
+		try {
+			s1 = new Sudoku(MySquare);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int [] Col = s1.getColumn(1);
+		int [] originalArray = Col;
+		int [] shuffledArray = shuffleArray(Col);
+	assertTrue(Arrays.equals(originalArray,shuffledArray));
+	}
+	// if assert true fails then the method was shuffled
+	private int[] shuffleArray(int[] col) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
